504cb484423ef525f472126fe1047e31c4610f69
oashraf@synapse-analytics.io <oashraf@synapse-analytics.io>
2020-11-14 11:57:14 +0200

    Added Public Key Input

--- secureMailApp/authentication/forms.py
+++ secureMailApp/authentication/forms.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
-
+    publickey = forms.CharField(max_length=255, label='Public Key',required=True)
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "password1", "password2", "publickey")
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
