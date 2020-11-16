f4f2adf2f95b0433f8c01f35ec57041404e6543e
user <aliaakhalifa@aucegypt.edu>
2020-11-16 14:00:34 +0200

    added keygeneration

--- secureMailApp/authentication/forms.py
+++ secureMailApp/authentication/forms.py
@@ -2,9 +2,30 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from Crypto import Random
+from Crypto.PublicKey import RSA
+import base64
+
+
+def encrypt_message(a_message , publickey):
+	encrypted_msg = publickey.encrypt(a_message.encode('utf-8'), 32)[0]
+	encoded_encrypted_msg = base64.b64encode(encrypted_msg) # base64 encoded strings are database friendly
+	return encoded_encrypted_msg
+
+def decrypt_message(encoded_encrypted_msg, privatekey):
+	decoded_encrypted_msg = base64.b64decode(encoded_encrypted_msg)
+	decoded_decrypted_msg = privatekey.decrypt(decoded_encrypted_msg)
+	return decoded_decrypted_msg.decode("utf-8")
+
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    publickey = forms.CharField(max_length=255, label='Public Key',required=True)
+    modulus_length = 256*4 # use larger value in production
+	privatekey = RSA.generate(modulus_length, Random.new().read)
+	publickey = privatekey.publickey()
+    f = open ("public.txt", "w")
+    f.write(publickey.exportKey())
+    f.close()
+    #publickey = forms.CharField(max_length=255, label='Public Key',required=True)
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2", "publickey")
@@ -15,3 +36,5 @@ class NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+    
+    
